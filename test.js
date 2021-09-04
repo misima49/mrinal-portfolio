7ec8b82b3c49d9d972746597f865cbed2835611d
misima49 <56494010+misima49@users.noreply.github.com>
2021-09-04 20:13:33 +0530

    Add contact me and footer

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ var typed = new Typed('#typed', {
     loop: true,
     typeSpeed: 100,
     backSpeed: 20,
-    showCursor: true
+    showCursor: true,
+    smartBackspace: false,
 });
 
 document.addEventListener('aos:in:skill-bars', ({ detail }) => {
@@ -64,9 +65,9 @@ particlesJS("particles-js", {
         "random": false,
         "anim": {
           "enable": false,
-          "speed": 0.5,
+          "speed": 1,
           "opacity_min": 0.1,
-          "sync": false
+          "sync": true
         }
       },
       "size": {
@@ -76,12 +77,12 @@ particlesJS("particles-js", {
           "enable": false,
           "speed": 40,
           "size_min": 0.1,
-          "sync": false
+          "sync": true
         }
       },
       "line_linked": {
         "enable": true,
-        "distance": 150,
+        "distance": 100,
         "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
@@ -140,22 +141,20 @@ particlesJS("particles-js", {
   });
   
   
-  /* ---- stats.js config ---- */
-  
-  var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector('.js-count-particles');
-  update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
+  // var count_particles, stats, update;
+  // stats = new Stats;
+  // stats.setMode(0);
+  // stats.domElement.style.position = 'absolute';
+  // stats.domElement.style.left = '0px';
+  // stats.domElement.style.top = '0px';
+  // document.body.appendChild(stats.domElement);
+  // count_particles = document.querySelector('.js-count-particles');
+  // update = function() {
+  //   stats.begin();
+  //   stats.end();
+  //   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+  //     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  //   }
+  //   requestAnimationFrame(update);
+  // };
+  // requestAnimationFrame(update);
