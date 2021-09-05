58522d1dd32c6e4baf00a44e614cf36a13901bad
misima49 <56494010+misima49@users.noreply.github.com>
2021-09-05 08:51:13 +0530

    Modify styles

--- test.js
+++ test.js
@@ -1,160 +1,143 @@
 var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 20,
-    showCursor: true,
-    smartBackspace: false,
+  stringsElement: '#typed-strings',
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 20,
+  showCursor: true,
+  smartBackspace: false,
 });
 
 document.addEventListener('aos:in:skill-bars', ({ detail }) => {
-    $(".progress-bar").each(function(i) {
-        $(this).delay(500 * i).animate({
-            width: $(this).attr('aria-valuenow') + '%'
-        }, 500);
-    });
+  $(".progress-bar").each(function (i) {
+    $(this).delay(500 * i).animate({
+      width: $(this).attr('aria-valuenow') + '%'
+    }, 500);
+  });
 });
 
-$(".nav-item i").mouseover(function(e) {
-    console.log($(e.target).text());
-    switch($(e.target).text()) {
-        case "home": $("<span> Home</span>").insertAfter($(e.target));
-                            break;
-        case "engineering": $("<span> Skills</span>").insertAfter($(e.target));
-                            break;
-        case "description": $("<span> About</span>").insertAfter($(e.target));
-                            break;
-        case "code": $("<span> Projects</span>").insertAfter($(e.target));
-                            break;
-    }
-}).mouseout(function(e) {
-    console.log($(e.target).next().remove());
+$(".nav-item i").mouseover(function (e) {
+  console.log($(e.target).text());
+  switch ($(e.target).text()) {
+    case "home": $("<span> Home</span>").insertAfter($(e.target));
+      break;
+    case "engineering": $("<span> Skills</span>").insertAfter($(e.target));
+      break;
+    case "description": $("<span> About</span>").insertAfter($(e.target));
+      break;
+    case "code": $("<span> Projects</span>").insertAfter($(e.target));
+      break;
+    case "email": $("<span> Contact me</span>").insertAfter($(e.target));
+      break;
+  }
+}).mouseout(function (e) {
+  console.log($(e.target).next().remove());
 })
 
 /* ---- particles.js config ---- */
 
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 100,
-        "density": {
-          "enable": true,
-          "value_area": 1000
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": true
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": true
-        }
-      },
-      "line_linked": {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
         "enable": true,
-        "distance": 100,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "value_area": 1000
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    "retina_detect": true
-  });
-  
-  
-  // var count_particles, stats, update;
-  // stats = new Stats;
-  // stats.setMode(0);
-  // stats.domElement.style.position = 'absolute';
-  // stats.domElement.style.left = '0px';
-  // stats.domElement.style.top = '0px';
-  // document.body.appendChild(stats.domElement);
-  // count_particles = document.querySelector('.js-count-particles');
-  // update = function() {
-  //   stats.begin();
-  //   stats.end();
-  //   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-  //     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  //   }
-  //   requestAnimationFrame(update);
-  // };
-  // requestAnimationFrame(update);
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": true
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": true
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 100,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
